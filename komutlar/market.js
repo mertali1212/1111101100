cdfe1ba80cd78005efb7a086a3bd27ad235de7d5
mertali1212 <99125788+mertali1212@users.noreply.github.com>
2022-02-08 23:28:39 +0300

    Add files via upload

---
+++ komutlar/market.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js')
+const db = require('quick.db');
+
+
+exports.run = async (client, message, args) => {
+  
+  if (!args[0]) return message.reply(`Mevcut Markettekiler : `(  
+    
+	if (args[0] === 'Apple') {
+    let para = await db.get(`para_${message.author.id}`) 
+    let fiyatcık = 1000 // istediğiniz fiyat
+    
+    if (para < fiyatcık) return message.reply('Yeterli Paran Yok!')
+    
+    db.set(`ìştebişiler_${message.author.id}`, "aktifdir")
+    db.add(`para_${message.author.id}`, -fiyatcık)
+    
+    return message.reply(`Ürünü Başarıyla Aldınız`)
+  }
+
+  
+  if (args[0] === 'Araba') {
+    let para = await db.get(`para_${message.author.id}`) 
+    let fiyatcık = 10000 // istediğiniz fiyat
+    
+    if (para < fiyatcık) return message.reply('Yeterli Paran Yok!')
+    
+    db.set(`ìştebişiler_${message.author.id}`, "aktifdir")
+    db.add(`para_${message.author.id}`, -fiyatcık)
+    
+    return message.reply(`Ürünü Başarıyla Aldınız`)
+  }
+
+
+  
+}
+exports.conf = {
+  
+    aliases: [],
+    permLevel: 0
+}
+
+exports.help = {
+    name: 'market'
+}
